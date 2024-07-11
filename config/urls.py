7633cc447ebf65ed763c0464d7871fa836596b4c
1F10230214 <S1F102302136@iniad.org>
2024-07-11 13:36:03 +0900

    削除機能の追加

--- config/urls.py
+++ config/urls.py
@@ -23,3 +23,12 @@ urlpatterns = [
     path('', todo_views.index, name='index'),
     path('<int:task_id>/', todo_views.detail, name='detail'),
 ]
+from django.contrib import admin
+from django.urls import path
+from todo import views as todo_views
+urlpatterns = [
+    path('admin', admin.site.urls),
+    path('', todo_views.index, name='index'),
+    path('<int:task_id>/', todo_views.detail, name='detail'),
+    path('<int:task_id>/delete', todo_views.delete, name='delete'),
+]
